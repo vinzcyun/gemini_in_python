cef369b27e830d0738ddac0f9c8d8530676c2167
duma <126393227+vinzcyun@users.noreply.github.com>
2024-05-14 09:25:11 +0700

    Update

---
+++ gemini_1.5.py
@@ -0,0 +1,23 @@
+import google.generativeai as genai
+
+print("Gemini chạy bằng Python được viết bởi VinZ")
+print("GitHub: vinzcyun")
+print("Phiên bản Gemini Pro 1.5")
+api_key = input("Nhập API Gemini Trong Google Cloud Studio: ")
+genai.configure(api_key=api_key)
+
+print("Đang kết nối đến API của Gemini...")
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+
+print("Đã kết nối")
+convo = model.start_chat(history=[])
+
+while True:
+    user_input = input("Nhập câu hỏi (chỉ bằng tiếng Anh): ")
+    convo.send_message(user_input)
+    print("Gemini trả lời:",convo.last.text)
+
+    if user_input.lower() == "quit" or user_input.lower() == "exit":
+        break
+
+print("Đoạn trò chuyện kết thúc!")
